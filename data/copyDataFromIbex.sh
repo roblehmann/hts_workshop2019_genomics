7a77e938fda00f768beb1e1cc1ac54c2877af64f
Robert Lehman <lehmanr@kl-18076.kaust.edu.sa>
2019-07-15 15:01:37 +0300

    fix bug in copying data from ibex folder

--- data/copyDataFromIbex.sh
+++ data/copyDataFromIbex.sh
@@ -6,7 +6,7 @@ cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/*k2d .
 
 # copy gene annotation, protein sequences, genome seq. for comparison of our results
 echo "copying reference genome files..."
-cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/GCF_000005845.2_ASM584v2 .
+cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/GCF_000005845.2_ASM584v2* .
 
 # get busco gene set database to assess completeness of our assmbly candidates
 echo 'copying busco files...'
